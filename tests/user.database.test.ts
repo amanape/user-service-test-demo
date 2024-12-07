39f0a3faefbd1221b3438d14dfd38be921a5a816
amanape <83104063+amanape@users.noreply.github.com>
2024-12-07 12:31:12 +0400

    Refactor

--- tests/user.database.test.ts
+++ tests/user.database.test.ts
@@ -1,8 +1,14 @@
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { Database } from "bun:sqlite";
 import { UserSQLiteDatabase } from "../src/user.database";
 
 describe("UserSQLiteDatabase", () => {
+  let db: UserSQLiteDatabase;
+
+  beforeEach(() => {
+    db = new UserSQLiteDatabase();
+  });
+
   it("should instantiate a table if it doesn't exist", () => {
     vi.spyOn(Database.prototype, "exec");
     new UserSQLiteDatabase();
@@ -13,32 +19,27 @@ describe("UserSQLiteDatabase", () => {
 
   it("should insert the user and return true", () => {
     const user = { id: "1", username: "fulan", name: "Fulan al Fulani" };
-    const db = new UserSQLiteDatabase();
     expect(db.insert(user)).toEqual(true);
   });
 
   it("should throw an error when inserting a user with the same id", () => {
     const user = { id: "1", username: "fulan", name: "Fulan al Fulani" };
-    const db = new UserSQLiteDatabase();
     db.insert(user);
     expect(() => db.insert(user)).toThrow();
   });
 
   it("should find the user by id", () => {
     const user = { id: "1", username: "fulan", name: "Fulan al Fulani" };
-    const db = new UserSQLiteDatabase();
     db.insert(user);
     expect(db.findById(user.id)).toEqual(user);
   });
 
   it("should return null when user is not found", () => {
-    const db = new UserSQLiteDatabase();
     expect(db.findById("non-existent-id")).toBeNull();
   });
 
   it("should update the user by id", () => {
     const user = { id: "1", username: "fulan", name: "Fulan al Fulani" };
-    const db = new UserSQLiteDatabase();
     db.insert(user);
     const newUser = db.update(user.id, {
       username: "johndoe",
@@ -53,13 +54,11 @@ describe("UserSQLiteDatabase", () => {
   });
 
   it("should throw an error when updating a non-existent user", () => {
-    const db = new UserSQLiteDatabase();
     expect(() => db.update("non-existent-id", { name: "John Doe" })).toThrow();
   });
 
   it("should delete the user by id", () => {
     const user = { id: "1", username: "fulan", name: "Fulan al Fulani" };
-    const db = new UserSQLiteDatabase();
     db.insert(user);
     const success = db.delete(user.id);
 
